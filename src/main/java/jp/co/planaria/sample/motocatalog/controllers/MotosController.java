38f5b49f70fb9f551fecf93b33abeed4fdbfe986
web_roaster <84627640+webroaster@users.noreply.github.com>
2023-01-23 19:50:39 +0900

    [update] アプリ設定をyml形式

--- src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
+++ src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
@@ -42,7 +42,7 @@ public class MotosController {
     model.addAttribute("brands", brands);
     model.addAttribute("motos", motos);
 
-    log.info("motos: {}", motos); // ログ出力
+    log.debug("motos: {}", motos); // ログ出力
 
     return "moto_list";
   }
