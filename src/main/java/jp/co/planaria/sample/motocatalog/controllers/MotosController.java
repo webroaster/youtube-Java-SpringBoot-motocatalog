4c82952b7c36b8c89fc534ba38a9d24b62dc22de
web_roaster <84627640+webroaster@users.noreply.github.com>
2023-01-27 17:55:56 +0900

    [update] 項目の書式設定

--- src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
+++ src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
@@ -1,5 +1,6 @@
 package jp.co.planaria.sample.motocatalog.controllers;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,8 +56,10 @@ public class MotosController {
     motos = service.getMotos(searchForm);
 
     model.addAttribute("motos", motos);
+    model.addAttribute("datetime", LocalDateTime.now());
 
     log.debug("motos: {}", motos); // ログ出力
+
 
     return "moto_list";
   }
